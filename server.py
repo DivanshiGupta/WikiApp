c49653994093e804efd9d23712a5802843532cd8
DivanshiGupta <divanshi0110@gmail.com>
2020-07-29 18:36:17 +0530

    updated

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template ,request
+from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField
+from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 from wikipedia import wikipedia
 
@@ -11,35 +11,41 @@ app.config['SECRET_KEY'] = 'some?bamboozle#string-foobar'
 Bootstrap(app)
 app.config['BOOTSTRAP_SERVE_LOCAL'] = True
 
+
 class TitleForm(FlaskForm):
-    title = StringField('Add title of page',validators=[Required()])
+    title = StringField('Add title of page', validators=[Required()])
     submit = SubmitField('Search')
 
-@app.route('/',methods=['GET','POST'])
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
     form = TitleForm()
-    message=''
-    search =''
+    message = ''
+    search = ''
     if form.validate_on_submit():
         title = form.title.data
         search = wikipedia.search(title)
-        y =[]
-        print(search)
+        y = []
         if search == y:
             search = 'This Wikipedia page does not exists.'
-            message=''
-        else :
-            search ='This Wikipedia page already exists. Following is a summary of the same'
-            message = wikipedia.summary(title) 
-    return render_template('index.html',form=form,message=message,search=search)
+            message = ''
+        else:
+            try:
+                message = wikipedia.summary(search[0])
+            except wikipedia.DisambiguationError as e:
+                s = e.options[0]
+                message = wikipedia.summary(s)
+            search = 'This Wikipedia page already exists. Following is a summary of the same'
+            # message = wikipedia.summary(title)
+    return render_template('index.html', form=form, message=message, search=search)
 
 # # @app.route('/add', methods=['POST'])
 # @app.route('/add',methods=('GET','POST'))
 
 # def add():
-    
+
 #     return render_template('add.html')
 
 
-if __name__ == '__main__' :
-    app.run(debug = True)
+if __name__ == '__main__':
+    app.run(debug=True)
